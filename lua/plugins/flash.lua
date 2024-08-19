b6d5d1777f29f89aef1f027bca6137f5a4034a18
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2024-08-19 20:33:33 +0200

    Add keymaps for flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,6 +3,27 @@
 return {
   "folke/flash.nvim",
   event = "CmdlineEnter",
+  init = function()
+    vim.keymap.set({ "n", "x", "o" }, "<leader>jj", function()
+      require("flash").jump()
+    end, { desc = "Flash | Jump" })
+
+    vim.keymap.set({ "n", "x", "o" }, "<leader>jt", function()
+      require("flash").treesitter()
+    end, { desc = "Flash | Treesitter" })
+
+    vim.keymap.set("o", "r", function()
+      require("flash").remote()
+    end, { desc = "Flash | Remote" })
+
+    vim.keymap.set({ "o", "x" }, "R", function()
+      require("flash").treesitter_search()
+    end, { desc = "Flash | Treesitter Search" })
+
+    -- vim.keymap.set({ "c" }, "<C-s>", function()
+    --   require("flash").toggle()
+    -- end, { desc = "Toggle Flash Search" })
+  end,
   opts = {
     labels = "asdfghjklqwertyuiopzxcvbnm",
     search = {
