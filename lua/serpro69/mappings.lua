2064d68c2118f7fb93742445a7c9966df71cbb3f
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-09-27 10:03:36 +0200

    Add mapping for closing all buffers

--- lua/serpro69/mappings.lua
+++ lua/serpro69/mappings.lua
@@ -7,3 +7,5 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "General | Exit Insert Mode" })
 vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "General | Save Buffer", silent = true })
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "General | Clear Search Highlights" })
 vim.keymap.set("n", "<leader>cs", "<cmd><CR>", { desc = "General | Clear Statusline" })
+-- credits: https://stackoverflow.com/questions/4545275/vim-close-all-buffers-but-this-one
+vim.keymap.set("n", "<leader>bd", "<cmd>%bd|e#|bd#<CR>|'\"", { desc = "General | Close All Buffers but Current" })
