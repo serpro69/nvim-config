ed88a7645e096c4ca0a48a043f82b7ad9f8f77f4
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-09-27 15:01:39 +0200

    Use markdownlint for formatting markdown files with conform

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -118,6 +118,11 @@ M.mason = {
     -- "tailwindcss-language-server",
     -- "emmet_language_server",
     -- "eslint-lsp",
+    -- "prettier",
+
+    -- Markdown / Notes
+    "marksman",
+    "markdownlint",
 
     -- PHP
     -- "intelephense",
@@ -141,7 +146,6 @@ M.mason = {
 
     -- Go
     "delve",
-    "go-debug-adapter",
     "goimports",
     "goimports-reviser",
     "golangci-lint",
