fc14240600031250b37cac44049d316cac9a4794
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-09-30 18:38:34 +0200

    Remap keys to close current buffer
    Use the same first key 'b' for buffer operations.
    Set <leader>bc to slose current buffer,
    instead of <leader>c which I'm miss-clicking way too often :D

--- lua/serpro69/mappings.lua
+++ lua/serpro69/mappings.lua
@@ -1,11 +1,25 @@
 -- NOTE: My personal mappings in a separate file for easier upstream syncing
 
--- General
+--- General
 
 vim.keymap.set("i", "jj", "<ESC>", { desc = "General | Exit Insert Mode" })
 vim.keymap.set("i", "jk", "<ESC>", { desc = "General | Exit Insert Mode" })
 vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "General | Save Buffer", silent = true })
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "General | Clear Search Highlights" })
-vim.keymap.set("n", "<leader>cs", "<cmd><CR>", { desc = "General | Clear Statusline" })
+
+-- NOTE: <leader>b for Buffer-related mappings
+--
+-- delete original <leader>c for closing the buffer
+vim.keymap.del("n", "<leader>c")
+-- and set it to <leader>bc
+vim.keymap.set("n", "<leader>bc", "<cmd>Bdelete!<cr>", { desc = "General | Close Buffer", silent = true })
+-- set <leader>bd to close all buffers but the current one
 -- credits: https://stackoverflow.com/questions/4545275/vim-close-all-buffers-but-this-one
-vim.keymap.set("n", "<leader>bd", "<cmd>%bd|e#|bd#<CR>|'\"", { desc = "General | Close All Buffers but Current" })
+vim.keymap.set(
+  "n",
+  "<leader>bd",
+  "<cmd>%bd|e#|bd#<cr>|'\"",
+  { desc = "General | Close All Buffers but Current", silent = true }
+)
+
+vim.keymap.set("n", "<leader>cs", "<cmd><CR>", { desc = "General | Clear Statusline" })
