6bce7ce08b76dc64aee74e07eaa51593cfc7dfda
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-10-02 18:44:25 +0200

    Update eldritch theme with latest from mgastonportillo/nvchad-config
    And port over a few other options/tricks from the same configs

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,3 +1,4 @@
+-- ref: https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 -- NOTE: NvChad Related Options
 ---@type ChadrcConfig
 -- This file needs to have same structure as nvconfig.lua
@@ -199,62 +200,40 @@ M.base46 = {
 -- check core.mappings for table structure
 -- M.mappings = require "mappings"
 
-local themed_values = require("serpro69.chadrc_aux").theme_customs[M.base46.theme]
-
 M.base46.hl_override = {
-  CursorLineNr = { fg = "yellow", bold = true },
-  FloatBorder = { fg = themed_values.border_fg, bg = "NONE" },
-  TelescopeBorder = { link = "FloatBorder" },
-  TelescopePromptBorder = { link = "FloatBorder" },
-  TelescopeSelection = { bg = themed_values.curline_bg, fg = "#DDDDDD", bold = true },
-  NvimTreeCursorLine = { bg = themed_values.curline_bg, fg = themed_values.curline_fg, bold = true },
-  MatchWord = { fg = "NONE", bg = "black2" },
-  MatchBackground = { link = "MatchWord" },
-  NeogitDiffContext = { bg = "NONE" },
-  NeogitDiffContextCursor = { bg = themed_values.curline_colour, bold = true },
-  NeogitDiffContextHighlight = { bg = "NONE" },
-  NvimTreeRootFolder = { fg = "vibrant_green" },
-  NvimTreeGitDirty = { link = "NvimTreeNormal" },
-  TbBufOn = { link = themed_values.buf_on_link },
-  St_NormalMode = { fg = themed_values.st_normal_fg, bg = themed_values.st_bg },
-  St_InsertMode = { fg = themed_values.st_insert_fg, bg = themed_values.st_bg },
-  St_cwd = { fg = themed_values.st_cwd_fg, bg = themed_values.st_bg },
-  St_CommandMode = { bg = themed_values.st_bg },
-  St_ConfirmMode = { bg = themed_values.st_bg },
-  St_SelectMode = { bg = themed_values.st_bg },
-  St_VisualMode = { bg = themed_values.st_bg },
-  St_ReplaceMode = { bg = themed_values.st_bg },
-  St_TerminalMode = { bg = themed_values.st_bg },
-  St_NTerminalMode = { bg = themed_values.st_bg },
-  -- BUG: (types) fg can take a table as an argument
-  TbBufOffModified = { fg = { "green", "black", 50 } }, ---@diagnostic disable-line
-  FloatTitle = { fg = themed_values.border_fg, bg = "NONE" },
-  LspInlayHint = { fg = themed_values.border_fg, bg = "NONE" },
-  Comment = { fg = themed_values.comment_fg, italic = true },
+  DevIconMd = { fg = "#FFFFFF", bg = "NONE" },
+  FloatTitle = { link = "FloatBorder" },
   CmpBorder = { link = "FloatBorder" },
   CmpDocBorder = { link = "FloatBorder" },
+  TelescopeBorder = { link = "FloatBorder" },
+  TelescopePromptBorder = { link = "FloatBorder" },
+  NeogitDiffContext = { bg = "NONE" },
+  NeogitDiffContextHighlight = { bg = "NONE" },
+  -- BUG: (types) fg accepts a table
+  TbBufOffModified = { fg = { "green", "black", 50 } }, ---@diagnostic disable-line
   ["@comment"] = { link = "Comment" },
   ["@keyword"] = { italic = true },
 }
 
 M.base46.hl_add = {
-  -- BUG: (types) fg can take a table as an argument
-  BeardedCurline = { fg = { "black", -12 } }, ---@diagnostic disable-line
+  YanVisual = { bg = "lightbg" },
   LspInfoBorder = { link = "FloatBorder" },
-  YankVisual = { bg = "lightbg" },
+  NvimTreeGitStagedIcon = { fg = "vibrant_green" },
   St_HarpoonInactive = { link = "StText" },
   St_HarpoonActive = { link = "St_LspHints" },
-  CodeActionSignHl = { fg = themed_values.code_action_fg },
-  NvimTreeGitStagedIcon = { fg = "vibrant_green" },
   MarkviewLayer2 = { bg = "#171B21" },
   MarkviewCode = { link = "MarkviewLayer2" },
   HelpviewCode = { link = "MarkviewLayer2" },
   HelpviewInlineCode = { link = "MarkviewInlineCode" },
   HelpviewCodeLanguage = { link = "MarkviewCode" },
+  CodeActionSignHl = { fg = "#F9E2AF" },
+  ["@number.luadoc"] = { fg = "Comment" },
   ["@markup.quote.markdown"] = { bg = "NONE" },
   ["@markup.raw.block.markdown"] = { link = "MarkviewLayer2" },
-  ["@number.luadoc"] = { fg = "Comment" },
 }
+
+local theme_customs = require("serpro69.chadrc_aux").themes_customs[M.base46.theme]
+M.base46 = theme_customs and vim.tbl_deep_extend("force", M.base46, theme_customs) or M.base46
 
 M.colorify = {
   enabled = true,
