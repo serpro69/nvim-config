2e876e2457fd17b8e7592d80d6a2cfc7c0642155
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-10-06 14:41:04 +0200

    Add autocmd for nvdash cursor position

--- lua/core/serpro69/autocommands.lua
+++ lua/core/serpro69/autocommands.lua
@@ -11,3 +11,14 @@ autocmd("FileType", {
     end
   end,
 })
+
+-- -- Set cursor position for nvdash
+-- -- credits to @gale ( https://discord.com/channels/869557815780470834/869557816430563370/1287435062769356830 )
+-- autocmd("FileType", {
+--   pattern = "nvdash",
+--   callback = function()
+--     if vim.bo.ft == "nvdash" then
+--       vim.api.nvim_win_set_cursor(0, { 18, 52 })
+--     end
+--   end,
+-- })
