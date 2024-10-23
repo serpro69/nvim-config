a017a7d87e82ba16a24bc69fcbc4e9ca3f851a30
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-10-23 10:15:25 +0200

    Ensure css, html and ts lsp are installed with mason

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -132,9 +132,9 @@ M.mason = {
     "trivy",
 
     -- Web Development
-    -- "css-lsp",
-    -- "html-lsp",
-    -- "typescript-language-server",
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
     -- "deno",
     -- "vue-language-server",
     -- "tailwindcss-language-server",
@@ -181,6 +181,7 @@ M.mason = {
   },
 }
 
+-- Disable signature help because it conflicts with folke/noice.nvim plugin
 M.lsp = { signature = false }
 
 M.base46 = {
