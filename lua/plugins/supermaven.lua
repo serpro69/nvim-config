a3f8c149385e13a5e5549975394648fddb4ddb5e
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-01-13 08:41:47 +0100

    Disable supermaven plugin

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,6 +2,7 @@
 -- NOTE: AI Autocomplete
 return {
   "supermaven-inc/supermaven-nvim",
+  cond = false,
   -- commit = "df3ecf7",
   -- commit = "40bde487fe31723cdd180843b182f70c6a991226",
   event = {
