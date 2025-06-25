5a557ebc2d58591bf42802d52c5334e51bd9cb59
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-06-25 10:04:26 +0200

    Add yamlls custom tags for mkdocs

--- lua/plugins/lsp/settings/yamlls.lua
+++ lua/plugins/lsp/settings/yamlls.lua
@@ -12,6 +12,18 @@ return {
         require("schemastore").yaml.schemas(),
         { kubernetes = { "*.k8s.yml", "*.k8s.yaml" } }
       ),
+      customTags = {
+        -- mkdocs
+        -- ref: https://www.mkdocs.org/user-guide/configuration/#environment-variables
+        -- ref: https://squidfunk.github.io/mkdocs-material/creating-your-site/#minimal-configuration
+        "!ENV scalar",
+        "!ENV sequence",
+        "!relative scalar",
+        "tag:yaml.org,2002:python/name:material.extensions.emoji.to_svg",
+        "tag:yaml.org,2002:python/name:material.extensions.emoji.twemoji",
+        "tag:yaml.org,2002:python/name:pymdownx.superfences.fence_code_format",
+        "tag:yaml.org,2002:python/object/apply:pymdownx.slugs.slugify mapping",
+      },
     },
   },
 }
