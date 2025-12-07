6a208517bf6f4f7965a4bc6e3413b9851b07188a
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-12-07 19:55:20 +0100

    Set lazy-lock path relative to nvim config dir

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,7 +45,7 @@ require("lazy").setup({
     enabled = true,
     notify = false, -- get a notification when changes are found
   },
-  lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
   performance = {
     rtp = {
       -- disable some rtp plugins
