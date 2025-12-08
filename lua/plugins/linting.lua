6882c264dfe272d646e3a9076236bc3e3aaeaa50
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-12-08 15:25:59 +0100

    (linting) Try to fix tofu linter for cases where root dir is not available
    E.g. when working on standalone modules, .terraform dir won't be found, and tofu validate will report errors

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -3,25 +3,29 @@ local tofu = require("lint").linters.tofu
 return {
   {
     "mfussenegger/nvim-lint",
+    event = "LspAttach",
     optional = true,
-    opts = {
-      linters = {
-        tofu = function()
-          local linter = tofu()
-          -- this still returns null for some reason
-          -- local root_dir = vim.lsp.buf.list_workspace_folders()[1]
-          local ok, lspconfig = pcall(require, "lspconfig")
-          if ok then
-            local root_dir = lspconfig.util.root_pattern("\\.terraform")
+    opts = function(_, opts)
+      opts.linters.tofu = function()
+        local linter = tofu()
+        -- this still returns null for some reason
+        -- local root_dir = vim.lsp.buf.list_workspace_folders()[1]
+        local ok, lspconfig = pcall(require, "lspconfig")
+        if ok then
+          local root_pattern = lspconfig.util.root_pattern("\\.terraform")
+          local root_dir = root_pattern(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.:h"))
+          if root_dir ~= nil then
             linter.args = {
-              "-chdir=" .. root_dir(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.:h")),
+              "-chdir=" .. root_dir,
               "validate",
               "-json",
             }
+          else
+            linter.cmd = "echo"
           end
-          return linter
-        end,
-      },
-    },
+        end
+        return linter
+      end
+    end,
   },
 }
