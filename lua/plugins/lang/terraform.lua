86643c9ab03cf09c08f816b8bc4623ef91307859
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-12-08 10:09:49 +0100

    (lang) Customize linter and formatter for terraform
    The defaults use terraform_validate, which fails and reports errors
    when using tofu. So instead, use tflint and trivy

---
+++ lua/plugins/lang/terraform.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        -- tofu linter incorrectly detects the directory of the opened file,
+        -- which results in errors for modules that it things are not installed
+        -- See: https://github.com/LazyVim/LazyVim/issues/6851
+        -- terraform = { "tofu", "tflint", "trivy" },
+        -- tf = { "tofu", "tflint", "trivy" },
+        terraform = { "tflint", "trivy" },
+        tf = { "tflint", "trivy" },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        hcl = { "packer_fmt" },
+        terraform = { "tofu_fmt" },
+        tf = { "tofu_fmt" },
+        ["terraform-vars"] = { "tofu_fmt" },
+      },
+    },
+  },
+}
