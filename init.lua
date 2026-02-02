f952f05d4374ed7b4d5897182e65afccfd9bd7ef
serpro69 <22973227+serpro69@users.noreply.github.com>
2026-02-02 15:32:31 +0100

    Add personal config overrides

--- init.lua
+++ init.lua
@@ -7,3 +7,6 @@ if vim.g.vscode then
   vim.opt.shortmess:append("s")
   vim.opt.cmdheight = 1
 end
+
+-- Late overrides (after plugins load): keymaps, autocmds, etc.
+pcall(require, "config.overrides")
