8d5ef35992ef0f60aaf6a6d40c65e3352e4c4179
serpro69 <22973227+serpro69@users.noreply.github.com>
2026-02-03 07:03:32 +0100

    (editor) Add window-picker plugin

---
+++ lua/plugins/editor/window-picker.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+-- NOTE: File Explorer window picker
+return {
+  "s1n7ax/nvim-window-picker",
+  name = "window-picker",
+  event = "VeryLazy",
+  version = "2.*",
+  opts = {
+    hint = "floating-big-letter",
+    show_prompt = false,
+  },
+}
