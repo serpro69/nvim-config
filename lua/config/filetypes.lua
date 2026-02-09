9af029a970d96da8dffb44bcb30c38fc0ca2a91e
serpro69 <22973227+serpro69@users.noreply.github.com>
2026-02-09 13:10:01 +0100

    Update ftdetect filetypes
    Add filetypes from ftdetect.
    Not removing ftdetect for now, some thing don't work well.
    E.g. docker compose filetype isn't set via filytypes, but is set via ftdetect...

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -5,6 +5,25 @@ vim.filetype.add({
     ["jinja"] = "htmldjango",
   },
   pattern = {
+    -- ansible files
+    [".*/main.ya?ml"] = "yaml.ansible",
+    [".*/site.ya?ml"] = "yaml.ansible",
+    [".*/ansible/.*%.ya?ml"] = "yaml.ansible",
+    [".*/defaults/.*%.ya?ml"] = "yaml.ansible",
+    [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
+    [".*/playbook.*%.ya?ml"] = "yaml.ansible",
+    [".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+    [".*/tasks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/molecule/.*%.ya?ml"] = "yaml.ansible",
+    -- docker files
+    [".*/.*dockerfile"] = "dockerfile",
+    [".*/.*Dockerfile"] = "dockerfile",
+    [".*/docker-compose%.ya?ml"] = "yaml.docker-compose",
+    -- github workflow files
     [".*/%.github[%w/]+workflows[%w/]+.*%.ya?ml"] = "yaml.github",
   },
 })
