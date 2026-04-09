59c535bc7374747e88e265e86199bdddd736f2a1
Alexis12120 <corporal461@gmail.com>
2026-04-10 06:55:39 +0800

    feat(plugin): add `nvumi`.

---
+++ lua/plugins/integration/nvumi.lua
@@ -0,0 +1,30 @@
+---@type LazySpec
+-- NOTE: Natural Language Calculator
+return {
+  "josephburgess/nvumi",
+  event = "VeryLazy",
+  opts = {
+    virtual_text = "newline", -- or "inline"
+    prefix = " = ", -- prefix shown before the output
+    date_format = "iso", -- or: "uk", "us", "long"
+    width = 0.4, -- 0–1 = fraction of terminal width, >1 = absolute columns
+    height = 0.4, -- 0–1 = fraction of terminal height, >1 = absolute lines
+    keys = {
+      run = "<CR>", -- run/refresh calculations
+      reset = "R", -- reset buffer
+      yank = "<leader>y", -- yank output of current line
+      yank_all = "<leader>Y", -- yank all outputs
+    },
+    -- see below for more on custom conversions/functions
+    custom_conversions = {},
+    custom_functions = {},
+  },
+  keys = {
+    {
+      "<leader>N",
+      "<cmd>Nvumi<cr>",
+      desc = "Natural Language Calculator",
+      silent = true,
+    },
+  },
+}
