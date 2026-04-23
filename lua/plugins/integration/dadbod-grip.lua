632ee3468ac666b26561701b1a249444fffb182a
serpro69 <22973227+serpro69@users.noreply.github.com>
2026-04-23 16:04:16 +0200

    Work around broken lazy.lua in dadbod-grip plugin pkg
    The plugin's lazy.lua returns a bare spec fragment without a plugin
    name, which fails lazy.nvim's pkg normalization. Add build hook to
    remove it on install/update and specify cmd list directly.

--- lua/plugins/integration/dadbod-grip.lua
+++ lua/plugins/integration/dadbod-grip.lua
@@ -4,9 +4,37 @@ return {
   {
     "joryeugene/dadbod-grip.nvim",
     version = "*",
+    -- plugin's lazy.lua is incompatible with lazy.nvim pkg system
+    build = function(plugin)
+      os.remove(plugin.dir .. "/lazy.lua")
+    end,
     opts = {
       completion = false,
-      keymaps = { qpad_execute = "<CR>" }, -- or whatever key you prefer
+      keymaps = { qpad_execute = "<CR>" },
+    },
+    cmd = {
+      "Grip",
+      "GripStart",
+      "GripHome",
+      "GripConnect",
+      "GripSchema",
+      "GripTables",
+      "GripQuery",
+      "GripSave",
+      "GripLoad",
+      "GripHistory",
+      "GripProfile",
+      "GripExplain",
+      "GripAsk",
+      "GripDiff",
+      "GripCreate",
+      "GripDrop",
+      "GripRename",
+      "GripProperties",
+      "GripExport",
+      "GripAttach",
+      "GripDetach",
+      "GripOpen",
     },
     keys = {
       { "<leader>D", false },
