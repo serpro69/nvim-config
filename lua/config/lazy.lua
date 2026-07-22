4d7462737b7d6100f6322469248e8c84b9cbdf4a
Alexis12119 <corporal461@gmail.com>
2026-07-22 21:19:35 +0800

    feat(plugin): disable netrw.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -53,7 +53,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         -- "tutor",
