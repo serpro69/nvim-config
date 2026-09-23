514fdb7fab75940207548b788847d59001029650
serpro69 <22973227+serpro69@users.noreply.github.com>
2026-09-23 14:27:04 +0200

    Override render-markdown to have it disabled by default

---
+++ lua/plugins/editor/render-markdown.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+-- NOTE: Markdown rendering
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    enabled = false,
+  },
+}
